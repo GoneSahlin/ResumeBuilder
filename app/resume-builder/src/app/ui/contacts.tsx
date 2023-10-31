d9029ed156d53e681d618e53b8419fda8b760616
GoneSahlin <zach@sahlins.net>
2023-10-31 03:33:22 -0700

    can add contacts

--- app/resume-builder/src/app/ui/contacts.tsx
+++ app/resume-builder/src/app/ui/contacts.tsx
@@ -2,23 +2,38 @@ import { Inputs } from "@/app/cv/page"
 import { useState } from "react";
 import { UseFormRegister } from "react-hook-form";
 
+
+const contactTypes: { [name: string]: {[item: string]: string}} = {
+  "phone": {addString: "Add Phone Number", labelString: "Phone Number:", inputType: "text"},
+  "email": {addString: "Add Email", labelString: "Email:", inputType: "email"},
+  "github": {addString: "Add GitHub URL", labelString: "GitHub URL:", inputType: "url"},
+  "linkedin": {addString: "Add LinkedIn URL", labelString: "LinkedIn URL:", inputType: "url"}
+}
+
 export function Contacts(register: UseFormRegister<Inputs>) {
-  const [contactTypes, setContactTypes] = useState<string[]>([]);
+  const [otherContactTypes, setOtherContactTypes] = useState<string[]>(Object.keys(contactTypes))
+  const [contactTypesUsed, setContactTypesUsed] = useState<string[]>([]);
   const [open, setOpen] = useState(false);
 
   const handleOpen = () => {
     setOpen(!open);
   }
 
-  function addContact() {
+  function addContact(name: string) {
     setOpen(!open);
-    setContactTypes([...contactTypes, "phone"]);
+    setContactTypesUsed([...contactTypesUsed, name]);
+
+    // remove item from otherContactTypes
+    const index = otherContactTypes.indexOf(name);
+    otherContactTypes.splice(index, 1);
+    setOtherContactTypes(otherContactTypes);
   }
 
-  function Contact() {
+  function Contact(name: string) {
+    const labelString = contactTypes[name].labelString;
     return (
       <div>
-        <label>Contact</label><br />
+        <label>{labelString}</label><br />
         <input {...register("phone")}></input>
       </div>
     )
@@ -26,12 +41,19 @@ export function Contacts(register: UseFormRegister<Inputs>) {
   
   return (
     <div>
-      {contactTypes.map(Contact)}
+      {contactTypesUsed.map(Contact)}
       <button onClick={handleOpen}>Add Contact</button>
       {open ? (
         <ul>
           <li>
-            <button onClick={addContact}>Add Phone Number</button>
+            {otherContactTypes.map((x) => {
+              const addString: string = contactTypes[x].addString;
+              return (
+                <div>
+                  <button onClick={() => addContact(x)}>{addString}</button><br />
+                </div>
+              );
+            })}
           </li>
         </ul>
       ) : null}
