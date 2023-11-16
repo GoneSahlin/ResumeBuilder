0e9880b2a6540445260ba7a51875d91b077c381a
GoneSahlin <zach@sahlins.net>
2023-11-16 00:09:54 -0800

    stores cv data in db

---
+++ front/src/app/api/store-cv.tsx
@@ -0,0 +1,25 @@
+"use server";
+
+import { currentUser } from '@clerk/nextjs';
+import { cleanData } from "./clean-data";
+import clientPromise from "./mongodb";
+
+export async function storeCV(data: any) {
+  // clean data
+  const doc = await cleanData(data);
+
+  // add userId to doc
+  const user = await currentUser();
+  const userId = user?.id;
+  doc["userId"] = userId;
+
+  // connection
+  const client = await clientPromise;
+  const db = client.db("resume_builder");
+  const cvs = db.collection("cvs");
+
+  // replace
+  const filter = {"userId": userId}
+  const options = { upsert: true };
+  cvs.replaceOne(filter, doc, options);
+}
--- front/src/app/cv/page.tsx
+++ front/src/app/cv/page.tsx
@@ -1,52 +1,9 @@
-"use client";
+import { CVForm } from "../ui/cv-form";
 
-import { useForm } from "react-hook-form"
-import { Contacts } from "../ui/contacts";
-import { Educations } from "../ui/educations";
-import { Projects } from "../ui/projects";
-import { Research } from "../ui/research";
-import { WorkExperiences } from "../ui/work-experience";
-import { RelatedCoursework } from "../ui/related-coursework";
-import { TechnicalSkills } from "../ui/technical-skills";
-import { cleanData } from "../lib/actions/clean-data";
 
-export type Inputs = {
-  firstName: string
-  lastName: string
-  phone: number
-  email: string
-  github: string
-  linkedin: string
-};
-
-export default function Page() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Inputs>({ shouldUnregister: true });
-  const onSubmit = (data: any) => cleanData(data);
-
+export default async function Page() {
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label>First Name:</label><br />
-      <input {...register("firstName", { required: true })} />
-      {errors.firstName && <span>This field is required</span>}<br />
-
-      <label>Last Name:</label><br />
-      <input {...register("lastName", { required: true })} />
-      {errors.lastName && <span>This field is required</span>}<br />
-
-      {Contacts(register)}<br />
-      {Educations(register)}<br />
-      {Projects(register)}<br />
-      {Research(register)}<br />
-      {WorkExperiences(register)}<br />
-      {RelatedCoursework(register)}<br />
-      {TechnicalSkills(register)}<br />
-
-      <input type="submit" value="Submit" /><br /><br />
-    </form>
+    <CVForm />
   );
 };
 
